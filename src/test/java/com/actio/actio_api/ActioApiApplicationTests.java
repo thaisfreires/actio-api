6c8540b24d1f3efb1a815fddfbd09762bcd7e3a3
Priscila C <priscilac@email.com>
2025-07-01 15:50:39 +0100

    setup

---
+++ src/test/java/com/actio/actio_api/ActioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actio.actio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
