215df99fc2ecb859544a763605816f08dd4404d4
Priscila C <priscila.campoos@yahoo.com.br>
2025-07-07 20:34:54 +0100

    refactor: Move WebClient DTOs

--- src/main/java/com/actio/actio_api/model/webclient/GlobalQuote.java
+++ src/main/java/com/actio/actio_api/model/webclient/GlobalQuote.java
@@ -1,4 +1,4 @@
-package com.actio.actio_api.model;
+package com.actio.actio_api.model.webclient;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
--- src/main/java/com/actio/actio_api/service/AlphaVantageWebClientService.java
+++ src/main/java/com/actio/actio_api/service/AlphaVantageWebClientService.java
@@ -1,7 +1,7 @@
 package com.actio.actio_api.service;
 
-import com.actio.actio_api.model.AlphaVantageResponse;
-import com.actio.actio_api.model.GlobalQuote;
+import com.actio.actio_api.model.webclient.AlphaVantageResponse;
+import com.actio.actio_api.model.webclient.GlobalQuote;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
