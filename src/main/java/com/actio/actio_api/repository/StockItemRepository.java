c50e8f339a7bfa83370c9b1119b282acf6cca2c8
Priscila C <priscila.campoos@yahoo.com.br>
2025-07-09 20:16:34 +0100

    create StockItemRepository

---
+++ src/main/java/com/actio/actio_api/repository/StockItemRepository.java
@@ -0,0 +1,7 @@
+package com.actio.actio_api.repository;
+
+import com.actio.actio_api.model.StockItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockItemRepository extends JpaRepository<StockItem, Integer> {
+}
