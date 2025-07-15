41a95d1c56afd98180fc395113bf04d5d580dde0
Priscila C <priscila.campoos@yahoo.com.br>
2025-07-15 09:47:16 +0100

    add findStockByStockName and existsStockByStockName on StockRepository

--- src/main/java/com/actio/actio_api/repository/StockRepository.java
+++ src/main/java/com/actio/actio_api/repository/StockRepository.java
@@ -3,5 +3,10 @@ package com.actio.actio_api.repository;
 import com.actio.actio_api.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StockRepository extends JpaRepository<Stock, Integer> {
+    List<Stock> findStockByStockName(String stockName);
+
+    boolean existsStockByStockName(String stockName);
 }
