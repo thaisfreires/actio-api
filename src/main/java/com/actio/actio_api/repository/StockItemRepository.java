f9647debf1762f00f5e4bf9695ff17619c53b618
Priscila C <priscila.campoos@yahoo.com.br>
2025-07-15 15:57:05 +0100

    fix StockItemRepository: replace Integer for StockItemId as Entity @Id type

--- src/main/java/com/actio/actio_api/repository/StockItemRepository.java
+++ src/main/java/com/actio/actio_api/repository/StockItemRepository.java
@@ -1,7 +1,9 @@
 package com.actio.actio_api.repository;
 
 import com.actio.actio_api.model.StockItem;
+import com.actio.actio_api.model.StockItemId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StockItemRepository extends JpaRepository<StockItem, Integer> {
+public interface StockItemRepository extends JpaRepository<StockItem, StockItemId> {
+
 }
