24ba86df02ecc1a3ee11c5140f70d0f6fa887df4
Thais <thaisfrsoares@gmail.com>
2025-07-16 14:38:01 +0100

    Implement Stock Item Service and Repo

--- src/main/java/com/actio/actio_api/repository/StockItemRepository.java
+++ src/main/java/com/actio/actio_api/repository/StockItemRepository.java
@@ -1,9 +1,9 @@
 package com.actio.actio_api.repository;
 
+import com.actio.actio_api.model.Account;
 import com.actio.actio_api.model.StockItem;
-import com.actio.actio_api.model.StockItemId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StockItemRepository extends JpaRepository<StockItem, StockItemId> {
-
+public interface StockItemRepository extends JpaRepository<StockItem, Integer> {
+    boolean existsByAccount(Account account);
 }
