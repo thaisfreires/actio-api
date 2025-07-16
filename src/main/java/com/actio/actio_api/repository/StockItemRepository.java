0bc68ccfa7e22d0981e1569a147ce7571f71fd92
Thais <thaisfrsoares@gmail.com>
2025-07-16 14:46:04 +0100

    Adjust implement StockItem

--- src/main/java/com/actio/actio_api/repository/StockItemRepository.java
+++ src/main/java/com/actio/actio_api/repository/StockItemRepository.java
@@ -3,7 +3,9 @@ package com.actio.actio_api.repository;
 import com.actio.actio_api.model.Account;
 import com.actio.actio_api.model.StockItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StockItemRepository extends JpaRepository<StockItem, Integer> {
     boolean existsByAccount(Account account);
 }
--- src/main/java/com/actio/actio_api/service/StockItemService.java
+++ src/main/java/com/actio/actio_api/service/StockItemService.java
@@ -1,8 +1,22 @@
 package com.actio.actio_api.service;
 
+import com.actio.actio_api.model.Account;
+import com.actio.actio_api.repository.StockItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StockItemService {
+    private final StockItemRepository stockItemRepository;
 
+    /**
+     * Checks if any StockItems exist for the given Account.
+     *
+     * @param account The account to check for associated StockItems.
+     * @return true if there is at least one StockItem for the account; false otherwise.
+     */
+    public boolean hasStockItemsForAccount(Account account) {
+        return stockItemRepository.existsByAccount(account);
+    }
 }
