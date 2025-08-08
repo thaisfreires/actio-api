c074acf01573817d736437011cbe1720e0eaa15b
pris-c <priscila.campoos@yahoo.com.br>
2025-08-08 17:22:27 +0100

    update StockTransactionResponse

--- src/main/java/com/actio/actio_api/model/response/StockTransactionResponse.java
+++ src/main/java/com/actio/actio_api/model/response/StockTransactionResponse.java
@@ -1,16 +1,22 @@
 package com.actio.actio_api.model.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 public class StockTransactionResponse {
     private Long transactionId;
+    private String transactionType;
+    private Integer transactionTypeId;
+    private String stockSymbol;
     private Long stockId;
     private Integer quantity;
     private BigDecimal value;
-    private BigDecimal totalValue;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime transactionDateTime;
 }
